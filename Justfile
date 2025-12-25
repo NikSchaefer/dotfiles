026d945a6470aaf9935af5070d3cd65769e825d8
Nik Schaefer <nikkschaefer@gmail.com>
2025-12-25 10:10:30 -0600

    feat: neovim update

--- Justfile
+++ Justfile
@@ -1,12 +1,10 @@
 install:
     @mkdir -p ~/.config
 
-    @# Symlink config directories
     @ln -sf ~/dotfiles/config/nvim ~/.config/nvim
     @ln -sf ~/dotfiles/config/ghostty ~/.config/ghostty
     @ln -sf ~/dotfiles/config/yazi ~/.config/yazi
 
-    @# Symlink config files
     @ln -sf ~/dotfiles/config/zsh/.zshrc ~/.zshrc
     @ln -sf ~/dotfiles/config/zsh/.p10k.zsh ~/.p10k.zsh
 
--- config/nvim/lua/plugin/plugins.lua
+++ config/nvim/lua/plugin/plugins.lua
@@ -2,211 +2,210 @@ local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 -- Color theme
 now(function()
-	add("folke/tokyonight.nvim")
-	require("tokyonight").setup({
-		transparent = true,
-		styles = {
-			sidebars = "transparent",
-			floats = "transparent",
-		},
-	})
-	vim.cmd([[colorscheme tokyonight]])
+    add("folke/tokyonight.nvim")
+    require("tokyonight").setup({
+        transparent = true,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+    })
+    vim.cmd([[colorscheme tokyonight]])
 end)
 
 -- Treesitter
 now(function()
-	add("nvim-treesitter/nvim-treesitter")
-	require("nvim-treesitter").setup({
-		ensure_installed = {
-			"rust",
-			"go",
-			"javascript",
-			"typescript",
-			"tsx",
-			"lua",
-			"html",
-			"css",
-			"markdown",
-			"markdown_inline",
-			"typst",
-			"toml",
-			"python",
-			"make",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-	})
+    add({ source = "nvim-treesitter/nvim-treesitter", checkout = "master" })
+    require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+            "rust",
+            "go",
+            "javascript",
+            "typescript",
+            "tsx",
+            "lua",
+            "html",
+            "css",
+            "markdown",
+            "markdown_inline",
+            "typst",
+            "toml",
+            "python",
+            "make",
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+        indent = {
+            enable = true,
+        },
+    })
 
-	add({
-		source = "nvim-treesitter/nvim-treesitter-textobjects",
-		checkout = "main",
-	})
+    add({
+        source = "nvim-treesitter/nvim-treesitter-textobjects",
+        checkout = "main",
+    })
 end)
 
 -- Auto pairs for HTML/JSX tags and general pairs
 later(function()
-	add("windwp/nvim-autopairs")
-	add("windwp/nvim-ts-autotag")
+    add("windwp/nvim-autopairs")
+    add("windwp/nvim-ts-autotag")
 
-	-- Setup auto-pairs
-	require("nvim-autopairs").setup({
-		check_ts = true,
-		ts_config = {
-			lua = { "string" },
-			javascript = { "string", "template_string" },
-			typescript = { "string", "template_string" },
-			tsx = { "string", "template_string" },
-			jsx = { "string", "template_string" },
-		},
-	})
-	-- Setup auto-tag for HTML/JSX
-	require("nvim-ts-autotag").setup({
-		opts = {
-			enable_close = true,
-			enable_rename = true,
-			enable_close_on_slash = true,
-		},
-	})
+    -- Setup auto-pairs
+    require("nvim-autopairs").setup({
+        check_ts = true,
+        ts_config = {
+            lua = { "string" },
+            javascript = { "string", "template_string" },
+            typescript = { "string", "template_string" },
+            tsx = { "string", "template_string" },
+            jsx = { "string", "template_string" },
+        },
+    })
+    -- Setup auto-tag for HTML/JSX
+    require("nvim-ts-autotag").setup({
+        opts = {
+            enable_close = true,
+            enable_rename = true,
+            enable_close_on_slash = true,
+        },
+    })
 end)
 
 -- LSP & Completion
 later(function()
-	-- Enable lsp configs
-	add({
-		source = "neovim/nvim-lspconfig",
-		depends = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
-	})
+    -- Enable lsp configs
+    add({
+        source = "neovim/nvim-lspconfig",
+        depends = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
+    })
 
-	-- Completion engine
-	add({
-		source = "saghen/blink.cmp",
-		depends = { "rafamadriz/friendly-snippets" },
-	})
-	require("blink.cmp").setup({
-		fuzzy = { implementation = "lua" },
-		keymap = { preset = "super-tab" },
-	})
+    -- Completion engine
+    add({
+        source = "saghen/blink.cmp",
+        depends = { "rafamadriz/friendly-snippets" },
+    })
+    require("blink.cmp").setup({
+        fuzzy = { implementation = "lua" },
+        keymap = { preset = "super-tab" },
+    })
 
-	-- Installer for LSP, formatters, linters, etc
-	add("williamboman/mason.nvim")
-	require("mason").setup()
-	-- Mute LSP progress notifications
-	vim.lsp.handlers["$/progress"] = function() end
-	-- Bridge for lsp configs to auto install from mason.nvim
-	add({
-		source = "williamboman/mason-lspconfig.nvim",
-		depends = { "williamboman/mason.nvim" },
-	})
-	require("mason-lspconfig").setup({
-		ensure_installed = {
-			"rust_analyzer",
-			-- "gopls",
-			"ts_ls",
-			"lua_ls",
-			"html",
-			"cssls",
-			"emmet_ls",
-			"tailwindcss",
-			"tinymist", -- Typst
-			"taplo", -- TOML
-			"ty", -- Python
-		},
-		automatic_installation = true,
-	})
-	-- Ensure installed formatters
-	add("WhoIsSethDaniel/mason-tool-installer.nvim")
-	require("mason-tool-installer").setup({
-		ensure_installed = {
-			"rustfmt",
-			"prettierd",
-			"stylua",
-			"goimports",
-			"typstyle",
-			"ruff", -- Python
-		},
-	})
-	-- Conform for formatting
-	add("stevearc/conform.nvim")
-	require("conform").setup({
-		formatters_by_ft = {
-			rust = { "rustfmt" },
-			go = { "goimports" },
-			javascript = { "prettierd" },
-			typescript = { "prettierd" },
-			javascriptreact = { "prettierd" },
-			typescriptreact = { "prettierd" },
-			json = { "prettierd" },
-			html = { "prettierd" },
-			css = { "prettierd" },
-			lua = { "stylua" },
-			typst = { "typstyle" },
-			toml = { "taplo" },
-			python = { "ruff" },
-		},
-		format_on_save = function()
-			-- Only format if it's NOT an autosave
-			if vim.g.is_autosave then
-				return false
-			end
-			return { lsp_fallback = true }
-		end,
-	})
+    -- Installer for LSP, formatters, linters, etc
+    add("williamboman/mason.nvim")
+    require("mason").setup()
+    -- Mute LSP progress notifications
+    vim.lsp.handlers["$/progress"] = function() end
+    -- Bridge for lsp configs to auto install from mason.nvim
+    add({
+        source = "williamboman/mason-lspconfig.nvim",
+        depends = { "williamboman/mason.nvim" },
+    })
+    require("mason-lspconfig").setup({
+        ensure_installed = {
+            "rust_analyzer",
+            -- "gopls",
+            "ts_ls",
+            "lua_ls",
+            "html",
+            "cssls",
+            "emmet_ls",
+            "tailwindcss",
+            "tinymist", -- Typst
+            "taplo",    -- TOML
+            "ty",       -- Python
+        },
+        automatic_installation = true,
+    })
+    -- Ensure installed formatters
+    add("WhoIsSethDaniel/mason-tool-installer.nvim")
+    require("mason-tool-installer").setup({
+        ensure_installed = {
+            "rustfmt",
+            "prettierd",
+            "stylua",
+            "goimports",
+            "typstyle",
+            "ruff", -- Python
+        },
+    })
+    -- Conform for formatting
+    add("stevearc/conform.nvim")
+    require("conform").setup({
+        formatters_by_ft = {
+            rust = { "rustfmt" },
+            go = { "goimports" },
+            javascript = { "prettierd" },
+            typescript = { "prettierd" },
+            javascriptreact = { "prettierd" },
+            typescriptreact = { "prettierd" },
+            json = { "prettierd" },
+            html = { "prettierd" },
+            css = { "prettierd" },
+            lua = { "stylua" },
+            typst = { "typstyle" },
+            toml = { "taplo" },
+            python = { "ruff" },
+        },
+        format_on_save = function()
+            -- Only format if it's NOT an autosave
+            if vim.g.is_autosave then
+                return false
+            end
+            return { lsp_fallback = true }
+        end,
+    })
 end)
 
 later(function()
-	add("rachartier/tiny-inline-diagnostic.nvim")
-	require("tiny-inline-diagnostic").setup()
-	vim.diagnostic.config({ virtual_text = false })
+    add("rachartier/tiny-inline-diagnostic.nvim")
+    require("tiny-inline-diagnostic").setup()
+    vim.diagnostic.config({ virtual_text = false })
 end)
 
 -- Harpoon
 later(function()
-	add({ source = "ThePrimeagen/harpoon", checkout = "harpoon2", depends = { "nvim-lua/plenary.nvim" } })
-	local harpoon = require("harpoon")
+    add({ source = "ThePrimeagen/harpoon", checkout = "harpoon2", depends = { "nvim-lua/plenary.nvim" } })
+    local harpoon = require("harpoon")
 
-	harpoon:setup()
+    harpoon:setup()
 
-	vim.keymap.set("n", "<leader>a", function()
-		harpoon:list():add()
-	end)
-	vim.keymap.set("n", "<C-e>", function()
-		harpoon.ui:toggle_quick_menu(harpoon:list())
-	end)
+    vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
+    end)
+    vim.keymap.set("n", "<C-e>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
 
-	vim.keymap.set("n", "<C-h>", function()
-		harpoon:list():select(1)
-	end)
-	vim.keymap.set("n", "<C-t>", function()
-		harpoon:list():select(2)
-	end)
-	vim.keymap.set("n", "<C-n>", function()
-		harpoon:list():select(3)
-	end)
-	vim.keymap.set("n", "<C-s>", function()
-		harpoon:list():select(4)
-	end)
+    vim.keymap.set("n", "<C-h>", function()
+        harpoon:list():select(1)
+    end)
+    vim.keymap.set("n", "<C-t>", function()
+        harpoon:list():select(2)
+    end)
+    vim.keymap.set("n", "<C-n>", function()
+        harpoon:list():select(3)
+    end)
+    vim.keymap.set("n", "<C-s>", function()
+        harpoon:list():select(4)
+    end)
 
-	-- Toggle previous & next buffers stored within Harpoon list
-	vim.keymap.set("n", "<C-S-P>", function()
-		harpoon:list():prev()
-	end)
-	vim.keymap.set("n", "<C-S-N>", function()
-		harpoon:list():next()
-	end)
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<C-S-P>", function()
+        harpoon:list():prev()
+    end)
+    vim.keymap.set("n", "<C-S-N>", function()
+        harpoon:list():next()
+    end)
 end)
 
 later(function()
-	add("folke/zen-mode.nvim")
-	require("zen-mode").setup({
-		window = {
-			width = 85,
-		},
-	})
-	vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>")
+    add("folke/zen-mode.nvim")
+    require("zen-mode").setup({
+        window = {
+            width = 85,
+        },
+    })
+    vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>")
 end)
-
