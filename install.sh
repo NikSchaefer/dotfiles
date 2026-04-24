4fc6d674db619df6c41f55c79a41b22e83efa6d6
Nik Schaefer <nikkschaefer@gmail.com>
2026-04-23 21:33:19 -0500

    feat: add btop theme

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config
 ln -sf ~/dotfiles/nvim ~/.config/nvim
 ln -sf ~/dotfiles/ghostty ~/.config/ghostty
 ln -sf ~/dotfiles/yazi ~/.config/yazi
+ln -sf ~/dotfiles/btop ~/.config/btop
 
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
