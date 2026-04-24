83bc98391d57ea2b4536170f25467b5469a5b44c
Nik Schaefer <nikkschaefer@gmail.com>
2026-04-24 00:07:33 -0500

    feat: add zap zsh to install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,8 @@ ln -sf ~/dotfiles/btop ~/.config/btop
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 
+# Install zap zsh
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
 echo "Dotfiles installed. 'source ~/.zshrc' to refresh"
 
