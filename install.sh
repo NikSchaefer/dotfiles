205656b71aa645337381df117cf1b8e71e227216
Nik Schaefer <nikkschaefer@gmail.com>
2026-04-27 12:39:06 -0500

    feat: ensure install.sh is idempotent

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
 mkdir -p ~/.config
 
-ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/ghostty ~/.config/ghostty
-ln -sf ~/dotfiles/yazi ~/.config/yazi
-ln -sf ~/dotfiles/btop ~/.config/btop
+# Use -n to prevent nesting if target is a symlink to a directory
+ln -snfv ~/dotfiles/nvim ~/.config/nvim
+ln -snfv ~/dotfiles/ghostty ~/.config/ghostty
+ln -snfv ~/dotfiles/yazi ~/.config/yazi
+ln -snfv ~/dotfiles/btop ~/.config/btop
 
-ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+ln -snfv ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -snfv ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 
-# Install zap zsh
-zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+# Install zap zsh if not already present
+if [ ! -d "$HOME/.local/share/zap" ]; then
+    curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh | zsh -s -- --branch release-v1
+else
+    echo "Zap Zsh already installed."
+fi
 
 echo "Dotfiles installed. 'source ~/.zshrc' to refresh"
 
