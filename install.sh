2f354258016e6ab97c58c2d3c2db9c21b7f7bf03
Nik Schaefer <nikkschaefer@gmail.com>
2026-04-28 15:33:46 -0500

    feat: update install.sh and brewfile; simplify yazi config

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.config
 
-# Use -n to prevent nesting if target is a symlink to a directory
+# -n to prevent nesting
 ln -snfv ~/dotfiles/nvim ~/.config/nvim
 ln -snfv ~/dotfiles/ghostty ~/.config/ghostty
 ln -snfv ~/dotfiles/yazi ~/.config/yazi
@@ -10,11 +10,9 @@ ln -snfv ~/dotfiles/btop ~/.config/btop
 ln -snfv ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -snfv ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 
-# Install zap zsh if not already present
+# Install zap zsh if not present
 if [ ! -d "$HOME/.local/share/zap" ]; then
     curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh | zsh -s -- --branch release-v1
-else
-    echo "Zap Zsh already installed."
 fi
 
 echo "Dotfiles installed. 'source ~/.zshrc' to refresh"
